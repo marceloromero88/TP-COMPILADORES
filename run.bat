51fe006d287a8a1903948102f62d138972221a55
marceloromero88 <49465481+marceloromero88@users.noreply.github.com>
2023-09-28 00:06:42 -0300

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,11 +1,7 @@
-:: Script para windows
 flex Lexico.l
 bison -dyv Sintactico.y
-
 gcc.exe lex.yy.c y.tab.c -o compilador.exe
-
 compilador.exe prueba.txt
-
 @echo off
 del compilador.exe
 del lex.yy.c
@@ -13,4 +9,3 @@ del y.tab.c
 del y.tab.h
 del y.output
 
-pause
